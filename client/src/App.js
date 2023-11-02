9ae57a2052d4f8ecedd55064ac25c387c8636e25
JackWSL <jgb1017@gmail.com>
2023-11-01 23:39:12 -0500

    Added copyright, reogranized logo, changed title

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
-import React from "react";
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import logo from './assets/logo.svg';
+import Copyright from './copyright';
+
+/*
+  Server runs on port 3001
+  - Can get the current build of the app on 3001
+  - Access the api at 3001:/api
+  - When start the react app on 3000, it still gets its info from the server at 3001:/api
+*/
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -10,7 +18,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
-  
+    
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +27,9 @@ function App() {
           {!data ? "Loading..." : data}
         </p>
       </header>
+      <body className='App-body'>
+        <Copyright year={2023}/>
+      </body>
     </div>
   );
 }
