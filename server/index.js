82bd3630b3e504e19128e24012983d195566e9aa
JackWSL <jgb1017@gmail.com>
2023-11-01 22:46:00 -0500

    Finished basic setup. At a starting point now

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
+const path = require("path");
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// For the api page
+// Node serve the files for our react app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
-// For the landing page
-app.get("", (req, res) => {
-    res.json({ message: "Welcome weary traveler!" });
-});
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+}); 
 
 // app.listen should be the bottom function
 app.listen(PORT, () => {
